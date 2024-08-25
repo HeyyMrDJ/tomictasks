0630c720a87bec705e61a0a9447b5f0f770a81ab
DJ <heyymrdj@protonmail.com>
2024-08-25 10:48:43 -0400

    Removing references to previous list and task structures

--- cmd/create.go
+++ cmd/create.go
@@ -34,42 +34,7 @@ var createNodeCmd = &cobra.Command{
 	},
 }
 
-var createTaskCmd = &cobra.Command{
-	Use:   "task [listName] [title] [description] [dueDate]",
-	Short: "Creates a new task",
-	Args:  cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-
-		if strings.Contains(args[0], "/") {
-			task := strings.Split(args[0], "/")
-			listName := task[0]
-			listID := database.GetListIDByName(db, listName)
-
-			fmt.Println("Creating task on listName: ", listName)
-			database.CreateTask(db, task[1], listID, "")
-		} else {
-			fmt.Println("Creating task on default list")
-			database.CreateTask(db, args[0], 1, "")
-			fmt.Println("Task created successfully")
-		}
-	},
-}
-
-var createListCmd = &cobra.Command{
-	Use:   "list [listName]",
-	Short: "Creates a new list",
-	Args:  cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-
-		fmt.Println("Creating list...", args[0])
-		database.CreateList(db, args[0])
-		fmt.Println("List created successfully")
-	},
-}
-
 func init() {
 	rootCmd.AddCommand(createCmd)
-	createCmd.AddCommand(createTaskCmd)
-	createCmd.AddCommand(createListCmd)
 	createCmd.AddCommand(createNodeCmd)
 }
